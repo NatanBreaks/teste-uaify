6afd8bf91f19e311cbafc58481c298f8d9e70ed0
Natã Lucas Silva <99276733+NatanBreaks@users.noreply.github.com>
2022-12-28 16:52:54 -0300

    Concluido

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var nome = document.getElementById("nome");
+var telefone = document.getElementById("telefone");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
+var produto = document.getElementById("produto");
+
+
 function alerta() {
-    alert ("Obridado por enviar!")
+    console.log(msgCheckbox)
+    if (nome.value != 0 && telefone.value != 0  && cidade.value != 0  && estado.value != 0  && produto.value != 0 ) {
+        alert ("Obridado por enviar!")
+
+    }
+    
 }
